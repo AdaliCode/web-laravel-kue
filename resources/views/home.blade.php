6f23d252befc615403b3abf7f22e56abd4462b42
AdaliCode <fiezayyan@gmail.com>
2024-08-01 18:38:48 +0700

    membuat tampilan awal home page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,20 @@
+@extends('layout.main')
+@section('container')
+{{-- Shop By Categories --}}
+    <svg style="color: orangered" xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-cart4" viewBox="0 0 16 16">
+        <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5M3.14 5l.5 2H5V5zM6 5v2h2V5zm3 0v2h2V5zm3 0v2h1.36l.5-2zm1.11 3H12v2h.61zM11 8H9v2h2zM8 8H6v2h2zM5 8H3.89l.5 2H5zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2m-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0m9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2m-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0"/>
+    </svg>
+    <h3>Shop By Categories</h3>
+    <div class="row g-3 mt-3">
+        @for ($i = 0; $i < 10; $i++)
+            <div class="col-md-4">
+                <div class="card" style="border-color: orangered">
+                    <img src="{{ asset('storage/roti.jpg') }}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                    <h5 class="card-title">breads</h5>
+                    </div>
+                </div>
+            </div>
+        @endfor
+    </div>
+@endsection
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,38 @@
+<div class="text-light" style="background-color: orangered">
+    <div class="container">
+        <footer class="py-5">
+          <div class="row">
+            <div class="col-6 col-md-2 mb-3">
+              <ul class="nav flex-column">
+                <li class="nav-item mb-3"><a href="#" class="nav-link p-0 text-light">Store Location</a></li>
+                <li class="nav-item mb-3"><a href="#" class="nav-link p-0 text-light">Disclaimer</a></li>
+                <li class="nav-item mb-3"><a href="#" class="nav-link p-0 text-light">Privacy Policy</a></li>
+              </ul>
+            </div>
+            <div class="col-6 col-md-2 mb-3">
+              <ul class="nav flex-column">
+                <li class="nav-item mb-3"><a href="#" class="nav-link p-0 text-light">Terms of Use</a></li>
+                <li class="nav-item mb-3"><a href="#" class="nav-link p-0 text-light">Contact Us</a></li>
+              </ul>
+            </div>
+            <div class="col-6 col-md-2 mb-3">
+              <ul class="nav flex-column">
+                <li class="nav-item mb-3"><a href="#" class="nav-link p-0 text-light">About Us</a></li>
+                <li class="nav-item mb-3"><a href="#" class="nav-link p-0 text-light">FAQ</a></li>
+              </ul>
+            </div>
+            <div class="col-md-5 offset-md-1 mb-3">
+              <form>
+                <h5>Subscribe to our newsletter</h5>
+                <p>Monthly digest of what's new and exciting from us.</p>
+                <div class="d-flex flex-column flex-sm-row w-100 gap-2">
+                  <label for="newsletter1" class="visually-hidden">Email address</label>
+                  <input id="newsletter1" type="text" class="form-control" placeholder="Email address">
+                  <button class="btn btn-primary" type="button">Subscribe</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </footer>
+    </div>
+</div>
